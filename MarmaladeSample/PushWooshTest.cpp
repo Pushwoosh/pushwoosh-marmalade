03f7130567e2e0eb6d299737553e309d1b62f831
diman <diman@pushwoosh.com>
2015-12-28 12:08:40 +0600

    Demo app ids

--- MarmaladeSample/PushWooshTest.cpp
+++ MarmaladeSample/PushWooshTest.cpp
@@ -4,24 +4,24 @@
 #include <string>
 
 std::string message;
-std::string PW_APPID="FE8B7-77BD4";
+std::string PW_APPID="4FC89B6D14A655.46488481";
 
 int32 OnPushRegistered(char* token, void* userData)
 {
-    if (token)
-    {
-        message += std::string("\n") + std::string(token);
-    }
-    else
-    {
-        message += std::string("\nError registering for push notifications");
-    }
-    return 0;
+	if (token)
+	{
+		message += std::string("\n") + std::string(token);
+	}
+	else
+	{
+		message += std::string("\nError registering for push notifications");
+	}
+	return 0;
 }
 
 int32 OnPushReceived(char* text, void* userData)
 {
-    message = std::string("Message: " + std::string(text));
+	message = std::string("Message: " + std::string(text));
 	return 0;
 }
 
@@ -33,9 +33,9 @@ int32 OnPushRegisterError(char* error, void* userData)
 // Main entry point for the application
 int main()
 {
-    message = "`xffffff";
+	message = "`xffffff";
 
-    if (s3ePushWooshNotificationsAvailable())
+	if (s3ePushWooshNotificationsAvailable())
 	{
 		s3ePushWooshRegister(S3E_PUSHWOOSH_REGISTRATION_SUCCEEDED, (s3eCallback)&OnPushRegistered, 0);
 		s3ePushWooshRegister(S3E_PUSHWOOSH_MESSAGE_RECEIVED, (s3eCallback)&OnPushReceived, 0);
@@ -44,9 +44,9 @@ int main()
 		//s3ePushWooshNotificationRegister();
 		const char* cstrApp = PW_APPID.c_str();
 		s3ePushWooshNotificationRegisterWithPWAppID(cstrApp);
-        s3ePushWooshStartLocationTracking();
-        
-        s3ePushWooshNotificationSetBadgeNumber(6);
+		s3ePushWooshStartLocationTracking();
+		
+		s3ePushWooshNotificationSetBadgeNumber(6);
 				
 		//Sample code for using local notifications
 		//Currently this is available for Android only. You can use default Marmalade extension for iOS local notifications at this time.
@@ -56,24 +56,24 @@ int main()
 		//s3ePushWooshScheduleLocalNotification("Your pumpkins are ready!", 30, 0);
 	}
 
-    // Wait for a quit request from the host OS
-    while (!s3eDeviceCheckQuitRequest())
-    {
-        // Fill background blue
-        s3eSurfaceClear(0, 0, 255);
+	// Wait for a quit request from the host OS
+	while (!s3eDeviceCheckQuitRequest())
+	{
+		// Fill background blue
+		s3eSurfaceClear(0, 0, 255);
 
-        // Print a line of debug text to the screen at top left (0,0)
-        // Starting the text with the ` (backtick) char followed by 'x' and a hex value
-        // determines the colour of the text.
+		// Print a line of debug text to the screen at top left (0,0)
+		// Starting the text with the ` (backtick) char followed by 'x' and a hex value
+		// determines the colour of the text.
 		s3eDebugPrint(120, 150, message.c_str(), 0);
 		// else
 		// 	s3eDebugPrint(120, 150, "`xffffffNot available :(", 0);
 
-        // Flip the surface buffer to screen
-        s3eSurfaceShow();
+		// Flip the surface buffer to screen
+		s3eSurfaceShow();
 
-        // Sleep for 0ms to allow the OS to process events etc.
-        s3eDeviceYield(0);
-    }
-    return 0;
+		// Sleep for 0ms to allow the OS to process events etc.
+		s3eDeviceYield(0);
+	}
+	return 0;
 }
